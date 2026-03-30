6105c5db8c536a3c9d4281ac7808d25ed68855fb
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2026-03-30 01:54:35 -0300

    Remove unnecessary line breaks in taskController.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,14 +1,14 @@
-const service = require('../services/taskService');
+const service = require('../services/taskService')
 
 function createTask(data) {
-    return service.addTask(data.title);
+    return service.addTask(data.title)
 }
 
 function listTasks() {
-    return service.getTasks();
+    return service.getTasks()
 }
 
 module.exports = {
     createTask,
     listTasks
-};
+}
